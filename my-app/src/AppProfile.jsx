b2b2b4ad02de6511865ca4566a2912f0efbd5934
junoshon <sanoldfox@naver.com>
2023-09-10 14:25:48 +0900

    profile 수정

--- my-app/src/AppProfile.jsx
+++ my-app/src/AppProfile.jsx
@@ -2,17 +2,24 @@ import "./App.css";
 import {Fragment} from "react";
 import Profile from "./components/Profile";
 import juno from "./juno.png";
-import sena from "./sena.png"
+import sena from "./sena.png";
 import Avatar from "./components/Avatar";
 
 
 function AppProfile() {
+  const handleclick = (e) => {
+    console.log(e);
+    alert("Clicked!");
+  };
   
   return (
     <Fragment>
+      <button onClick={handleclick}>버튼</button>
+      <form action="" onSubmit={() => {
+      }}></form>
       <Avatar image={juno} isNew={true}/>
-      <Profile image={juno} name={"JUNO"} title="Frone-end developer"isNew={true} />
-      <Profile image={sena}  name={"Sena"} title="developer" isNew={true}/>
+      <Profile image={juno} name={"JUNO"} title="Frone-end developer" isNew={true}/>
+      <Profile image={sena} name={"Sena"} title="developer" isNew={true}/>
     </Fragment>
   );
 }
