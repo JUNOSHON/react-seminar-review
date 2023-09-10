d184b63f9ed1a5b2285ff0c8f82da0cae6415ace
junoshon <sanoldfox@naver.com>
2023-09-10 14:35:00 +0900

    feat: Reset버튼 추가 및 setCount의 클로저 개념 이해

--- my-app/src/components/Counter.jsx
+++ my-app/src/components/Counter.jsx
@@ -8,9 +8,16 @@ export default function Counter() {
     <div className="counter">
       <span className="number">{num}</span>
       <button className="button" onClick={() => {
-        setNum(num + 1);
+        setNum((prev) => prev + 1);
+        setNum((prev) => prev + 1);
+        setNum((prev) => prev + 1);
       }
       }>+
+      </button>
+      <button className="resetbutton" onClick={() => {
+        setNum(num - num);
+      }
+      }>Reset
       </button>
     </div>
   );
