53b1a60c9c9da286196bd28f8c59474089f0f7ed
junoshon <sanoldfox@naver.com>
2023-09-27 00:23:56 +0900

    feat: 프로젝트 초기 설정

--- my-app/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import AppJSX from './AppJSX';
-
-test('renders learn react link', () => {
-  render(<AppJSX />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
