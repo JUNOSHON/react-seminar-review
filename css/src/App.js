14ef6e01b4bfbb070315a7a0823b7cafe79f1ab3
junoshon <sanoldfox@naver.com>
2023-10-10 18:51:18 +0900

    init: 프로젝트 초기 설정

---
+++ css/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import TailwindComponents from "./components/TailwindComponents";
+
+function App() {
+  return (
+    <>
+      <TailwindComponents/>
+    </>
+  );
+}
+
+export default App;
---
+++ css/src/components/TailwindComponents.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function TailwindComponents() {
+  return (
+    <div>
+      <h1 className="text-violet-500">Hello</h1>
+      <button className="bg-blue-500 ">Button</button>
+    </div>
+  );
+}
