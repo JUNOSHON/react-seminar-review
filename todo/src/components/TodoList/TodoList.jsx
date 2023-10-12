473943302fc3ed6d44b5f4a25b3c4a1ab0ba8416
junoshon <sanoldfox@naver.com>
2023-10-12 22:18:43 +0900

    design: 투두리스트 css 추가

--- todo/src/components/TodoList/TodoList.jsx
+++ todo/src/components/TodoList/TodoList.jsx
@@ -1,27 +1,36 @@
 import React, {useState} from "react";
 import AddTodo from "../AddTodo/AddTodo";
 import Todo from "../Todo/Todo";
+import styles from "./TodoList.module.css"
 
-export default function TodoList() {
+export default function TodoList({filter}) {
   const [todos, setTodos] = useState([
-    {id: "123", text: "장보기", stats: "active"},
-    {id: "124", text: "공부하기", stats: "active"},
+    {id: "123", text: "리액트 세미나", status: "active"},
+    {id: "124", text: "모던자바스크립트", status: "active"},
   ]);
   
-  const handleAdd = (todo) => {
-    setTodos([...todos, todo]);
-  };
+  const handleAdd = (todo) => setTodos([...todos, todo]);
+  
   const handleUpdate = (update) => setTodos(todos.map((t) => (t.id === update.id ? update : t)));
   const handleDelete = (deleted) => setTodos(todos.filter((t) => t.id !== deleted.id));
   
+  const filtered = getFilteredItems(todos, filter);
+  
   return (
-    <section>
-      <ul>
-        {todos.map((item) => (
-          <Todo key={item.id} todo = {item} onUpdate = {handleUpdate} onDelete = {handleDelete}/>
+    <section className={styles.container}>
+      <ul className={styles.list}>
+        {filtered.map((item) => (
+          <Todo key={item.id} todo={item} onUpdate={handleUpdate} onDelete={handleDelete}/>
         ))}
       </ul>
       <AddTodo onAdd={handleAdd}/>
     </section>
   );
+}
+
+function getFilteredItems(todos, filter) {
+  if (filter === "all") {
+    return todos;
+  }
+  return todos.filter((todo) => todo.status === filter);
 }
