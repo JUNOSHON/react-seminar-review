0c66dd349b3769f683f3f3b62f13b9e09cdd8668
junoshon <sanoldfox@naver.com>
2023-10-14 23:53:08 +0900

    feat: tailwind 컴포넌트 작성

--- css/src/components/TailwindComponents.jsx
+++ css/src/components/TailwindComponents.jsx
@@ -4,7 +4,7 @@ export default function TailwindComponents() {
   return (
     <div>
       <h1 className="text-violet-500">Hello</h1>
-      <button className="bg-blue-500 ">Button</button>
+      <button className="bg-blue-500">Button</button>
     </div>
   );
 }
