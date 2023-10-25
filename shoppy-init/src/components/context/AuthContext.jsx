dc8df99903547cf71970a516b6d52abdd8670418
junoshon <sanoldfox@naver.com>
2023-10-25 19:12:34 +0900

    feat: 경로 보호 설정

---
+++ shoppy-init/src/components/context/AuthContext.jsx
@@ -0,0 +1,26 @@
+import {createContext, useContext, useEffect, useState} from "react";
+import {login, logout, onUserStateChange} from "../../api/firebase";
+
+const AuthContext = createContext(null);
+
+export function AuthContextProvider({children}) {
+  const [user,setUser] = useState();
+  
+  useEffect(() => {
+    //컴포넌트가 마운트 될 때 사용자 상태가 어떤지 판단해서 user 상태에 넣어줌
+    onUserStateChange(user => {
+      console.log(user);
+      setUser(user);
+    });
+  }, []);
+  
+  return (
+    <AuthContext.Provider value={{user, login,logout}}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export function useAuthContext() {
+  return useContext(AuthContext);
+}
