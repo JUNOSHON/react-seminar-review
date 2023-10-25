9becf1808ec167942b83f66332092bbfaea218e6
junoshon <sanoldfox@naver.com>
2023-10-25 11:41:08 +0900

    feat: 로그인, 로그아웃 기능 추가

--- shoppy-init/src/components/Navbar.jsx
+++ shoppy-init/src/components/Navbar.jsx
@@ -1,22 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 import {Link} from "react-router-dom";
 import {FiShoppingBag} from "react-icons/fi";
 import {BsFillPencilFill} from "react-icons/bs";
+import {login, logout} from "../api/firebase";
 
 export default function Navbar() {
+  const [user, setUser] = useState();
+  const handleLogin = () => {
+    login().then(setUser);
+  };
+  const handleLogout = () => {
+    logout().then(setUser);
+  };
+  
   return (
-    <header className='flex justify-between border-b border-gray-300 p-4'>
+    <header className="flex justify-between border-b border-gray-300 p-4">
       <Link to="/" className="flex items-center text-4xl text-brand">
         <FiShoppingBag/>
         <h1>Shoppy!</h1>
       </Link>
-      <nav className='flex items-center gap-4 font-semibold'>
+      <nav className="flex items-center gap-4 font-semibold">
         <Link to="/products">Products</Link>
         <Link to="/carts">Carts</Link>
-        <Link to="/products/new" className='text-2xl'>
+        <Link to="/products/new" className="text-2xl">
           <BsFillPencilFill/>
         </Link>
-        <button>Login</button>
+        {user?<button onClick={handleLogout}>Logout</button>:<button onClick={handleLogin}>Login</button>}
       </nav>
     </header>
   );
